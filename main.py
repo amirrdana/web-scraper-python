ce67db62132f0d3945328ea799c32d700e50c5c2
Amir Dana <amirrdana@gmail.com>
2025-02-27 13:57:27 +0330

    scrape all of the website

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ if "https://" not in website and "www." in website:
     website = f"https://{website}"
 elif "https://" not in website and "www." not in website:
     website = f"https://www.{website}"
+elif "https://" in website and "www." not in website:
+    pass
+    # implement
 
 try :
     r = requests.get(website)
@@ -25,13 +28,21 @@ html_content = r.text
 soup = BeautifulSoup(html_content, 'html.parser')
 
 data = dict()
-data['links'] = [link.get('href') for link in soup.find_all('a') if link.get('href')]
-data['images'] = [img.get('src') for img in soup.find_all('img') if img.get('src')]
+for element in soup.find_all():
+    tag_name = element.name
+    if tag_name not in data:
+        data[tag_name] = []
+    element_data = {
+        'attribute': element.attrs,
+        'text': element.get_text(strip=True)
+    }
+    data[tag_name].append(element_data)
 
 # print(data)
 
-with open('output.csv', 'w') as out:
+with open('output.csv', 'w', newline='', encoding='utf-8') as out:
     writer = csv.writer(out)
-    writer.writerow(['Links', 'Images'])
-    for link, image in zip_longest(data['links'], data['images'], fillvalue=''):
-        writer.writerow([link, image])
+    writer.writerow(['Tag', 'Attributes', 'Text'])
+    for tag_name, elements in data.items():
+        for element in elements:
+            writer.writerow([tag_name, element['attribute'], element['text']])
