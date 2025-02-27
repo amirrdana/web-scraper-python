53b52eb168d33b141888319b53bea50ce7f6b7e3
Amir Dana <amirrdana@gmail.com>
2025-02-27 11:49:06 +0330

    error handling

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import csv
+import sys
 import requests
 from bs4 import BeautifulSoup
 from itertools import zip_longest
 
-r = requests.get("https://www.google.com")
+if len(sys.argv) != 2:
+    print("Usage: python main.py <website>")
+    sys.exit(1)
+
+website = sys.argv[1]
+if "https://" not in website and "www." in website:
+    website = f"https://{website}"
+elif "https://" not in website and "www." not in website:
+    website = f"https://www.{website}"
+
+try :
+    r = requests.get(website)
+    r.raise_for_status()
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching the webpage {website}: {e}")
+    sys.exit(1)
+print(f"Connection to {website} successful: {r.status_code}")
 html_content = r.text
 soup = BeautifulSoup(html_content, 'html.parser')
 
@@ -18,8 +35,3 @@ with open('output.csv', 'w') as out:
     writer.writerow(['Links', 'Images'])
     for link, image in zip_longest(data['links'], data['images'], fillvalue=''):
         writer.writerow([link, image])
-
-# for link in links:
-#     print(link.get('href'))
-# for image in imgs:
-#     print(image.get('src'))
