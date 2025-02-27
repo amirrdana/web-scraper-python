f0485bb12c814c5f03ba317f9cf75a56c2a0c4d7
Amir Dana <amirrdana@gmail.com>
2025-02-27 11:19:10 +0330

    Get all links and image sources

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import socket
+import csv
+import requests
+from bs4 import BeautifulSoup
+from itertools import zip_longest
 
-HOST = 'www.google.com'
-PORT = 80
+r = requests.get("https://www.google.com")
+html_content = r.text
+soup = BeautifulSoup(html_content, 'html.parser')
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = (HOST, PORT)
+data = dict()
+data['links'] = [link.get('href') for link in soup.find_all('a') if link.get('href')]
+data['images'] = [img.get('src') for img in soup.find_all('img') if img.get('src')]
 
-client_socket.connect(server_address)
+# print(data)
+
+with open('output.csv', 'w') as out:
+    writer = csv.writer(out)
+    writer.writerow(['Links', 'Images'])
+    for link, image in zip_longest(data['links'], data['images'], fillvalue=''):
+        writer.writerow([link, image])
+
+# for link in links:
+#     print(link.get('href'))
+# for image in imgs:
+#     print(image.get('src'))
