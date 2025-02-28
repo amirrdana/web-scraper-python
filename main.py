f96615a7da8e23b756aa6ccc40faa0c18aea19e5
Amir Dana <amirrdana@gmail.com>
2025-02-28 07:49:39 +0330

    Use user-agent headers to connect

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ if len(sys.argv) != 2:
     print("Usage: python main.py <website>")
     sys.exit(1)
 
+# Function to read headers from a file
+def read_headers_from_file(file_path):
+    with open(file_path, 'r') as file:
+        headers = [line.strip() for line in file if line.strip()]
+    return headers
+
 website = sys.argv[1]
 if "https://" not in website and "www." in website:
     website = f"https://{website}"
@@ -22,13 +28,28 @@ elif "https://" in website and "www." not in website:
         trailer = char
     website = "https://www." + website[stop_point:]
 
-try :
-    r = requests.get(website)
-    r.raise_for_status()
-except requests.exceptions.RequestException as e:
-    print(f"Error fetching the webpage {website}: {e}")
-    sys.exit(1)
-print(f"Connection to {website} successful: {r.status_code}")
+header_file = 'user_agents.txt'
+header_list = read_headers_from_file(header_file)
+
+# try to connect with user agent headers headers
+success = False
+for index, header in enumerate(header_list, start=1):
+    print(f"Trying header {index}...")
+    headers = {'User-Agent': header}
+    try:
+        r = requests.get(website, headers=headers)
+        r.raise_for_status
+        if (r.status_code == 200):
+            success = True
+            print(f"Connection to {website} successful with header {index}: {r.status_code}")
+            break
+    except requests.exceptions.RequestException as e:
+        print(f"Failed with header {index}, {e}")
+if not success:
+    print(f"Connection failed with status code: {r.status_code}")
+    print("Exiting program")
+    sys.exit()
+
 html_content = r.text
 soup = BeautifulSoup(html_content, 'html.parser')
 
